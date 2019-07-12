0a3fe48f2b15353f58111070b1e3352022e7a73b
DanielTisue <d.tisue@gmail.com>
2019-07-12 15:49:35 -0500

    Configure + require multer and cloudinary. Factor create route for cloudinary image upload option. Factor edit route for cloudinary image updated functionality. Factor delete route to delete cloudinary image along with post.

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,33 @@
 var express = require("express");
-var router  = express.Router();
+var router = express.Router({ mergeParams: true });
 var Post = require("../models/post");
 var middleware = require("../middleware");
 
 
+// Multer + Cloudinary Config
+
+const crypto = require("crypto");
+const multer = require("multer");
+const cloudinary = require("cloudinary");
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+const cloudinaryStorage = require("multer-storage-cloudinary");
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: "Ericas Blog",
+  allowedFormats: ["jpeg", "jpg", "png"],
+  filename: function(req, file, cb) {
+    let buf = crypto.randomBytes(16);
+    buf = buf.toString("hex");
+    let uniqFileName = file.originalname.replace(/\.jpeg|\.jpg|\.png/gi, "");
+    uniqFileName += buf;
+    cb(undefined, uniqFileName);
+  }
+});
+const upload = multer({ storage });
 
 
 //Show all posts
@@ -18,7 +42,16 @@ router.get("/", function(req, res){
  });
 
 //CREATE - add new post to DB
-router.post("/", middleware.isAdmin, function(req, res){
+router.post("/", middleware.isAdmin, upload.single("image"), function(req, res){
+    // set image req.
+   req.body.image = {
+     url: req.file.secure_url,
+     public_id: req.file.public_id
+   };
+   req.body.createdAt = {
+       type: Date,
+       default: Date.now
+   }
     // get data from form and add to posts array
     var name = req.body.name;
     var image = req.body.image;
@@ -27,12 +60,14 @@ router.post("/", middleware.isAdmin, function(req, res){
         id: req.user._id,
         username: req.user.username,
     };
-    var newPost = {name: name, image: image, description: description, author:author};
+    var date = req.body.createdAt;
+    var newPost = {name: name, image: image, description: description, date: date, author:author};
     //create new post
     Post.create(newPost, function(err, newlyCreated){
         if(err){
             console.log(err);
         } else {
+            console.log(newlyCreated);
             req.flash("success", "Blog post Successfully Created!");
             return res.redirect("/posts");
         }
@@ -50,6 +85,7 @@ router.get("/:id", function(req, res){
     //find the post with provided ID
     Post.findById(req.params.id).populate("comments").exec(function(err, foundPost){
         if(err){
+            req.flash("error", err.message );
             console.log(err);
         } else {
             console.log(foundPost);
@@ -64,18 +100,34 @@ router.get("/:id/edit", middleware.isAdmin, function(req, res){
     Post.findById(req.params.id, function(err, foundPost){
         if(err){
             console.log(err);
+            res.redirect("/posts")
         }
         res.render("posts/edit", {post: foundPost});
     });
 });
 
 // UPDATE POST ROUTE
-router.put("/:id", middleware.isAdmin, function(req, res){
+router.put("/:id", middleware.isAdmin, upload.single("image"), function(req, res){
     // find and update the correct post
-    Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, updatedPost){
+    Post.findByIdAndUpdate(req.params.id, req.body.post, async function(err, updatedPost){
       if(err){
+          console.log(err);
+          req.flash("error", "Something went wrong:" + err.message);
           res.redirect("/posts");
       } else {
+           if (req.file) {
+             try {
+               await cloudinary.v2.uploader.destroy(
+                 post.image.public_id
+               );
+               post.image.public_id = req.file.public_id;
+               post.image.url = req.file.secure_url;
+               await post.save();
+             } catch (err) {
+               return res.redirect("back");
+             }
+           }
+           req.flash("success", "Post successfully updated!");
           //redirect somewhere(show page)
           res.redirect("/posts/" + req.params.id);
       }
@@ -84,11 +136,14 @@ router.put("/:id", middleware.isAdmin, function(req, res){
 
 // DESTROY POST ROUTE
 router.delete("/:id", middleware.isAdmin, function(req, res){
-   Post.findByIdAndRemove(req.params.id, function(err){
-      if(err){
+   Post.findById(req.params.id, async function(err){
+      if(err) {
+          req.flash("error", "UH OH...Something went wrong:" + err.message);
           res.redirect("/posts");
       } else {
-          req.flash("success", "Blog post Successfully Deleted!");
+          await cloudinary.v2.uploader.destroy(post.image.public_id);
+          await post.remove();
+          req.flash("success", "Blog Post Successfully Deleted!");
           res.redirect("/posts");
       }
    });
